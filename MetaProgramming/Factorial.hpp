ddffda90ec345c37a07db918fbf92b4f074693f7
Sam Belliveau <sam.belliveau@gmail.com>
2018-11-25 17:29:10 -0500

    Add files via upload

---
+++ MetaProgramming/Factorial.hpp
@@ -0,0 +1,22 @@
+#ifndef SAMS_META_FACTORIAL_HPP
+#define SAMS_META_FACTORIAL_HPP 1
+
+#include "MetaHeader.hpp"
+
+namespace Meta{
+
+template<UInt_t Number>
+struct Factorial
+{
+  enum {
+    value = Number * Factorial<Number - 1>::value
+  };
+};
+
+template<>
+struct Factorial<0>
+{ enum { value = 1 }; };
+
+}
+
+#endif
