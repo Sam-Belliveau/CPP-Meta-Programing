e72a5a0c8a88ae13692a15f35543bb795f187bb4
Sam Belliveau <sam.belliveau@gmail.com>
2018-11-28 14:15:56 -0500

    Add files via upload

---
+++ MetaProgramming/Log2.hpp
@@ -0,0 +1,24 @@
+#ifndef SAMS_META_LOG_TWO_HPP
+#define SAMS_META_LOG_TWO_HPP 1
+
+#include "MetaHeader.hpp"
+#include "Power.hpp"
+
+namespace Meta
+{
+  template<UInt_t Number, UInt_t Power = sizeof(UInt_t)*8 - 1>
+  struct Log2
+  {
+  	enum {
+  		value = (Number & (UInt_t(1) << Power))
+  			? (Number & (UInt_t(1) << (Power - 1)))
+        ? Power + 1 : Power
+  			: Log2<Number, Power - 1>::value
+  	};
+  };
+
+  template<UInt_t Number>
+  struct Log2<Number, 0> { enum { value = 0 }; };
+}
+
+#endif
